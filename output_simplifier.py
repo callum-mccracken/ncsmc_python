e163445b3e366a3a25a291801e9c825fad110690
Callum McCracken <ccmccracken@oak.arc.ubc.ca>
2019-10-29 12:50:36 -0700

    fixed J, parity, T detection

--- output_simplifier.py
+++ output_simplifier.py
@@ -12,6 +12,8 @@ python output_simplifier.py -f [filename]
 
 """
 import argparse
+import re
+
 import utils
 
 # enter a filename here,
@@ -20,6 +22,8 @@ filename = "ncsmc_output/ncsm_rgm_Am2_1_1.out"
 
 # edit these two if you want to change the format of the output
 file_format = """Simplified View of {filename}:
+
+(only includes bound states)
 
 {states}
 =========================================================================
@@ -42,62 +46,111 @@ def simplify(filename):
     no more scrolling through 100000 line files!
     """
     filename = utils.abs_path(filename)
-    print("Simplifying", filename)
+    print("Simplifying "+filename)
     # get all lines from the file, as a list of strings
     with open(filename, "r+") as file_to_simplify:
         lines = file_to_simplify.readlines()
 
-    # bound state line will look like
-    # Bound state found at E_b=[energy] [unit]
-    bound_state_string = "Bound state found at E_b="
-    step = "looking for J and parity"
-    
+
     # parameters for each bound state
-    E, J, T, parity, details = "", "", "", "", ""
+    default = "ERROR"  # if something went wrong, we'll see this in the output
+    E, J, T, parity, details = default, default, default, default, default
 
     states = []
 
+    """
+    Steps:
+
+    1. Look for J, parity, T.
+        - we may have many of these values before seeing a bound state
+        - keep the most recent values before the bound state is mentioned
+    2. Get bound state energy
+    3. Get details
+    """
+    # start by searching for a bound state
+    step = "looking for bound state"
+
+    def j_parity_line(line):
+        """
+        checks if line is of the form 
+        
+        2*J=  6    parity=-1
+        """
+        regex = r"[ ]*2\*J=[ ]*[-]?[0-9]*[ ]*parity=[ ]*[-]?[0-9]*\n"
+        return bool(re.match(regex, line))
+    def get_j_parity(line):
+        """assuming j_parity_line(line) == True, return J, parity"""
+        # remove everything except necessary info
+        just_nums = line.replace("2*J=", "").replace("parity=", "")
+        # get the two "words", i.e. numbers, separated by spaces
+        Jx2, parity = just_nums.split()
+        J = int(Jx2)/2
+        return J, parity
+    def t_line(line):
+        """
+        checks if line is of the form 
+        
+        2*T= 0
+        """ 
+        regex = r"[ ]*2\*T=[ ]*[-]?[0-9]*\n"
+        return bool(re.match(regex, line))
+    def get_t(line):
+        """assuming t_line(line) == True, return T"""
+        Tx2 = line.replace("2*T=", "")
+        T = int(Tx2)/2
+        return T
+    def bound_state_line(line):
+        """
+        checks if line is of the form 
+        
+        Bound state found at E_b=[energy] [unit]
+        """ 
+        return "Bound state found at E_b=" in line
+    def get_e(line):
+        """assuming bound_state_line(line) == True, return E as a string"""
+        # remove the initial bit, as well as extra whitespace
+        E = line.replace("Bound state found at E_b=", "").strip()
+        return E
+
+
     for line in lines:
-        if step in ["looking for J and parity", "done"]:
-            if "2*J=" in line and "parity=" in line:
-                # the line looks like  2*J=  6    parity=-1
-                # remove unncessary junk
-                replaced = line.replace("2*J=", "").replace("parity=", "")
-                # get the two remaining "words", separated by spaces
-                Jx2, parity = replaced.split()
-                J = int(Jx2)/2
-                step = "looking for T"
-        if step == "looking for T":
-            if "2*T=" in line:
-                # the line looks like  2*T= 0
-                # remove unncessary junk
-                Tx2 = line.replace("2*T=", "")
-                T = int(Tx2)/2
-                step = "looking for bound state"
+        # get J, T, parity, E
         if step in ["looking for bound state", "done"]:
-            if bound_state_string in line:
-                # remove the initial bit, as well as extra whitespace
-                E = line.replace(bound_state_string, "").strip()
-                step = "looking for details"
-        if step == "looking for details":
+            # safe to assume that if we find a bound state, we'll find
+            # J, parity, T first, so no need to make that its own step
+            if j_parity_line(line):
+                J, parity = get_j_parity(line)
+            elif t_line(line):
+                T = get_t(line)
+            # then keep looking for a bound state.
+            # if we find new J, parity, T values we'll update as needed
+
+            elif bound_state_line(line):
+                E = get_e(line)
+                step = "looking for details" 
+
+        # get the first detail line
+        elif step == "looking for details":
             if "i_p,p_chan,p_st" in line:
-                details += line
+                details = line
                 step = "getting details"
-        if step == "getting details":
+        # get all other detail lines
+        elif step == "getting details":
             if "i_p,p_chan,p_st" in line:
                 details += line
             else:
+                # once we've found the first line after the details,
                 # save state and move on to the next one
                 states.append(state_format.format(
                     E=E, J=J, T=T, parity=parity, details=details))
-                # set some parameters back to "", but not all
+                # set some parameters back to default, but not all
                 # since some might be the same as for the next state
-                E, details = "", ""
-                # back to the first step!
-                step = "done"
+                E, details = default, default
+                # back to the first step / look for next bound state
+                step = "looking for bound state"
 
-    # ensure we ended on a good note
-    if step != "done":
+    # ensure we didn't end part way through processing a state
+    if step != "looking for bound state":
         raise IOError("unable to parse file correctly, exited at wrong step!")
 
     # write everything to a file
@@ -109,7 +162,7 @@ def simplify(filename):
     with open(filename+"_simplified", "w+") as out_file:
         out_file.write(file_str)
     print("Done simplifying!")
-    print("Output:", filename+"_simplified")
+    print("Output: "+filename+"_simplified")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Output Simplifier")
